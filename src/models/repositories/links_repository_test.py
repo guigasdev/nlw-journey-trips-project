6c3d500c82f29168b033961aaff1027e45f7b4fa
Guilherme Pereira <pgguilherme337@gmail.com>
2024-07-10 23:03:19 -0300

    feat: Implementing links repository

--- src/models/repositories/links_repository_test.py
+++ src/models/repositories/links_repository_test.py
@@ -6,26 +6,31 @@ from .links_repository import LinksRepository
 db_connection_handler.connect()
 
 trip_id  = str(uuid.uuid4())
+link_id  = str(uuid.uuid4())
 
-
+@pytest.mark.skip(reason="Interação com o banco")
 def test_registry_link():
     conn = db_connection_handler.get_connection()
     links_infos = LinksRepository(conn)
     
 
     links_trips_infos = {
-        "id": str(uuid.uuid4()),
+        "id": link_id,
         "trip_id": trip_id,
-        "link": "https://youtube.com"
+        "link": "https://youtube.com",
+        "title":"Viagem para a Orlando"
     }
 
     links_infos.registry_link(links_trips_infos)
 
-
+@pytest.mark.skip(reason="Interação com o banco")
 def test_find_links_from_trip():
     conn = db_connection_handler.get_connection()
     links_repository = LinksRepository(conn)
 
-    links = links_repository.find_emails_from_trip(trip_id)
+    links = links_repository.find_links_from_trip(trip_id)
     print()
     print(links)
+
+    assert isinstance(links, list)
+    assert isinstance(links[0], tuple)
